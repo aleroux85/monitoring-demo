b32b3eaf7aedd9735c98a5a01134c758a938ebad
Anro le Roux <anro@oligoden.com>
2022-10-16 22:07:27 +0200

    Update service-a

--- service-a/main.go
+++ service-a/main.go
@@ -5,12 +5,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"math/rand"
 	"net/http"
 	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	// "github.com/prometheus/client_golang/prometheus"
+	// "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type streams struct {
@@ -30,18 +28,44 @@ type Logx struct {
 
 func (l Logx) Log(lvl, msg string) error {
 	ts := fmt.Sprint(time.Now().UnixNano())
-	s := map[string]string{l.job: lvl}
-	v := []entry{entry{ts, msg}}
+	s := map[string]string{
+		"level": lvl,
+	}
+	v := []entry{{ts, msg}}
 
 	ss := streams{
 		Streams: []stream{
-			stream{
+			{
 				Stream: s,
 				Values: v,
 			},
 		},
 	}
 
+	return l.send(ss)
+}
+
+func (l Logx) LogError(msg string) error {
+	ts := fmt.Sprint(time.Now().UnixNano())
+	s := map[string]string{
+		"level":  "ERROR",
+		"system": "sysa",
+	}
+	v := []entry{{ts, msg}}
+
+	ss := streams{
+		Streams: []stream{
+			{
+				Stream: s,
+				Values: v,
+			},
+		},
+	}
+
+	return l.send(ss)
+}
+
+func (l Logx) send(ss streams) error {
 	b, err := json.Marshal(ss)
 	if err != nil {
 		return err
@@ -61,30 +85,33 @@ func (l Logx) Log(lvl, msg string) error {
 }
 
 func main() {
-	reqDuration := prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: "company_a",
-		Subsystem: "service_a",
-		Name:      "request_duration",
-		Help:      "Duration of requests.",
-	})
+	// reqDuration := prometheus.NewGauge(prometheus.GaugeOpts{
+	// 	Namespace: "company_a",
+	// 	Subsystem: "service_a",
+	// 	Name:      "request_duration",
+	// 	Help:      "Duration of requests.",
+	// })
 
-	go func() {
-		logx := Logx{"service_a"}
+	// go func() {
+	logx := Logx{"service_a"}
 
-		for {
-			reqDuration.Set(rand.NormFloat64()*10.0 + 400.0)
-			if err := logx.Log("INFO", "info message here"); err != nil {
-				fmt.Println(err)
-			}
-			time.Sleep(time.Second)
+	for {
+		// reqDuration.Set(rand.NormFloat64()*10.0 + 400.0)
+		// if err := logx.Log("INFO", "info message here"); err != nil {
+		// 	fmt.Println(err)
+		// }
+		if err := logx.LogError("error message here"); err != nil {
+			fmt.Println(err)
 		}
-	}()
-
-	if err := prometheus.Register(reqDuration); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("reqDuration registered.")
+		time.Sleep(time.Second)
 	}
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+	// }()
+
+	// if err := prometheus.Register(reqDuration); err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println("reqDuration registered.")
+	// }
+	// http.Handle("/metrics", promhttp.Handler())
+	// http.ListenAndServe(":2112", nil)
 }
